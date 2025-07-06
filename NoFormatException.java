d6bfad71799cf6187668a91b2f55b9480cd6c606
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-06 17:12:52 +0530

    Add files via upload

---
+++ NoFormatException.java
@@ -0,0 +1,11 @@
+package wrapper;
+
+public class NoFormatException {
+
+	public static void main(String[] args) {
+		
+		String s = "4fdyv254";
+		int a= Integer.parseInt(s);
+		System.out.println(a);
+	}
+}
