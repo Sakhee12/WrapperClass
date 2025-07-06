3423de6f44ecd8e9136c0a885e25b0b88a08e906
Sakhee12 <157628964+Sakhee12@users.noreply.github.com>
2025-07-06 17:13:28 +0530

    Add files via upload

---
+++ StaticFinalVar.java
@@ -0,0 +1,33 @@
+package wrapper;
+
+public class StaticFinalVar {
+
+	public static void main(String[] args) {
+		
+		System.out.println(Integer.MAX_VALUE);
+		System.out.println(Integer.MIN_VALUE);
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println(Byte.MAX_VALUE);
+		System.out.println(Byte.MIN_VALUE);
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println(Short.MAX_VALUE);
+		System.out.println(Short.MIN_VALUE);
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println(Long.MAX_VALUE);
+		System.out.println(Long.MIN_VALUE);
+		
+		System.out.println("-----------------------------");
+		
+		System.out.println(Float.MAX_EXPONENT);
+		System.out.println(Float.MAX_VALUE);
+		System.out.println(Float.MIN_EXPONENT);
+		System.out.println(Float.MIN_NORMAL);
+		System.out.println(Float.MIN_VALUE);
+	}
+}
